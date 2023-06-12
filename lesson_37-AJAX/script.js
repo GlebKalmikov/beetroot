0f6bce1b0a688da585e89f81e4666ac699744e21
Glib <gleb.kalmikov@gmail.com>
2023-06-12 22:35:54 +0300

    add correct

--- lesson_37-AJAX/script.js
+++ lesson_37-AJAX/script.js
@@ -1,76 +1,90 @@
-const apiKey = 'ced0220d'; 
+const apiKey = 'ced0220d';
 
-$(document).ready(function () {
-  $('#searchButton').click(function () {
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('searchButton').addEventListener('click', function () {
     searchMovies();
   });
 
-  $(document).on('click', '.movie-details-button', function () {
-    const imdbId = $(this).data('imdbid');
-    getMovieDetails(imdbId);
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('movie-details-button')) {
+      const imdbId = event.target.dataset.imdbid;
+      getMovieDetails(imdbId);
+    }
   });
 
-  $(document).on('click', '.pagination-button', function () {
-    const page = $(this).data('page');
-    searchMovies(page);
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('pagination-button')) {
+      const page = event.target.dataset.page;
+      searchMovies(page);
+    }
   });
 });
 
 function searchMovies(page = 1) {
-  const searchInput = $('#searchInput').val();
-  const type = $('#typeSelect').val();
+  const searchInput = document.getElementById('searchInput').value;
+  const type = document.getElementById('typeSelect').value;
   const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchInput}&type=${type}&page=${page}`;
 
-  $.ajax({
-    url: url,
-    type: 'GET',
-    dataType: 'json',
-    success: function (response) {
-      displayMovies(response);
-      displayPagination(response);
-    },
-    error: function (error) {
+  fetch(url)
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      displayMovies(data);
+      displayPagination(data);
+    })
+    .catch(function (error) {
       console.log(error);
-    }
-  });
+    });
 }
 
 function displayMovies(data) {
-  const moviesList = $('#moviesList');
-  moviesList.empty();
+  const moviesList = document.getElementById('moviesList');
+  moviesList.innerHTML = '';
 
   if (data.Search) {
-    data.Search.forEach(movie => {
-      const movieItem = $('<li>').addClass('movie-item');
-      const title = $('<div>').text(movie.Title);
-      const year = $('<div>').text(`Year: ${movie.Year}`);
-      const detailsButton = $('<button>')
-        .addClass('movie-details-button')
-        .attr('data-imdbid', movie.imdbID)
-        .text('Details');
+    data.Search.forEach(function (movie) {
+      const movieItem = document.createElement('li');
+      movieItem.classList.add('movie-item');
 
-      const movieDetails = $('<div>').addClass('movie-details');
+      const title = document.createElement('div');
+      title.textContent = movie.Title;
+
+      const year = document.createElement('div');
+      year.textContent = `Year: ${movie.Year}`;
+
+      const detailsButton = document.createElement('button');
+      detailsButton.classList.add('movie-details-button');
+      detailsButton.dataset.imdbid = movie.imdbID;
+      detailsButton.textContent = 'Details';
+
+      const movieDetails = document.createElement('div');
+      movieDetails.classList.add('movie-details');
 
       movieItem.append(title, year, detailsButton, movieDetails);
-      moviesList.append(movieItem);
+      moviesList.appendChild(movieItem);
     });
   } else {
-    moviesList.text('Movie not found!');
+    moviesList.textContent = 'Movie not found!';
   }
 }
 
 function displayPagination(response) {
   const totalResults = parseInt(response.totalResults);
   const totalPages = Math.ceil(totalResults / 10);
-  const pagination = $('#pagination');
-  pagination.empty();
+  const pagination = document.getElementById('pagination');
+  pagination.innerHTML = '';
 
   if (totalPages > 1) {
     for (let i = 1; i <= totalPages; i++) {
-      const button = $('<button class="pagination-button">')
-        .text(i)
-        .data('page', i);
-      pagination.append(button);
+      const button = document.createElement('button');
+      button.classList.add('pagination-button');
+      button.textContent = i;
+      button.dataset.page = i;
+      pagination.appendChild(button);
     }
   }
 }
@@ -78,30 +92,43 @@ function displayPagination(response) {
 function getMovieDetails(imdbId) {
   const url = `http://www.omdbapi.com/?apikey=${apiKey}&i=${imdbId}`;
 
-  $.ajax({
-    url: url,
-    type: 'GET',
-    dataType: 'json',
-    success: function (response) {
-      displayMovieDetails(response);
-    },
-    error: function (error) {
+  fetch(url)
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      displayMovieDetails(data);
+    })
+    .catch(function (error) {
       console.log(error);
-    }
-  });
+    });
 }
 
 function displayMovieDetails(movie) {
-  const details = $(`.movie-item .movie-details[data-imdbid="${movie.imdbID}"]`);
-  details.empty();
+  const details = document.querySelector(`.movie-item .movie-details[data-imdbid="${movie.imdbID}"]`);
+  details.innerHTML = '';
 
-  const poster = $('<img>').attr('src', movie.Poster);
-  const rating = $('<div>').text(`IMDb Rating: ${movie.imdbRating}`);
-  const plot = $('<div>').text(`Plot: ${movie.Plot}`);
-  const actors = $('<div>').text(`Actors: ${movie.Actors}`);
-  const genre = $('<div>').text(`Genre: ${movie.Genre}`);
-  const director = $('<div>').text(`Director: ${movie.Director}`);
+  const poster = document.createElement('img');
+  poster.setAttribute('src', movie.Poster);
+
+  const rating = document.createElement('div');
+  rating.textContent = `IMDb Rating: ${movie.imdbRating}`;
+
+  const plot = document.createElement('div');
+  plot.textContent = `Plot: ${movie.Plot}`;
+
+  const actors = document.createElement('div');
+  actors.textContent = `Actors: ${movie.Actors}`;
+
+  const genre = document.createElement('div');
+  genre.textContent = `Genre: ${movie.Genre}`;
+
+  const director = document.createElement('div');
+  director.textContent = `Director: ${movie.Director}`;
 
   details.append(poster, rating, plot, actors, genre, director);
-  details.show();
+  details.style.display = 'block';
 }
